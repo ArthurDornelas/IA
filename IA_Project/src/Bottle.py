b79a6797d64d5e1bd0831d05a6b8dbd650c77eaa
ArthurDornelas <arthurdornelas99@gmail.com>
2019-09-05 20:46:35 -0300

    Adição classes

---
+++ IA_Project/src/Bottle.py
@@ -0,0 +1,10 @@
+class Bottle:
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def transfer(self, otherBottle):
+        capacityOther = otherBottle.getCapacity
+
+    def getCapacity(self):
+        return self.capacity
+
